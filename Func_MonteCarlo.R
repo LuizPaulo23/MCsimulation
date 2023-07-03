258a7cddc630749923849a6b8fc05405717ad014
LuizPaulo23 <luizpauloueg@gmail.com>
2023-07-03 19:20:24 -0300

    CHANGED: refatorando

--- Func_MonteCarlo.R
+++ Func_MonteCarlo.R
@@ -2,44 +2,75 @@
 #' @author Luiz Paulo Tavares Gonçalves 
 #' @details Assumindo o seguinte modelo: Y_{i} = \beta_{1} + \beta_{2}X_{i} + \mu_{i} 
 
-rm(list = ls())
-graphics.off()
-set.seed(123)
-
-# \Packages
-library(tidyverse)
-library(DataExplorer)
-library(stargazer)
-library(stats)
-
-# Desenvolvendo a função: 
-
-MonteCarlo <- function(intercept, beta,n,repetitions){
+MonteCarlo <- function(intercept, beta,distribution,n,repetitions){
   
-  x = stats::rnorm(n) 
+  
   intercept_mc = numeric(repetitions)
   beta_mc = numeric(repetitions)
   
   for (i in 1:repetitions) {
     # Definindo a equação 
-    y <- intercept + beta * x + stats::rnorm(length(x), mean = 0, sd = 1)  # resíduos como normal-padrão
-    model_lm <- stats::lm(y ~ x)
-    intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
-    beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
     
-    MonteCarlo = list(intercept_mc, beta_mc)
+    if(distribution == "Gaussian"){
+      
+      x = stats::rnorm(n) 
+      y <- intercept + beta * x + stats::rnorm(length(x)) 
+      model_lm <- stats::lm(y ~ x)
+      intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+      beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+      
+      MonteCarlo = list(intercept_mc, beta_mc, x, y)
+      
+    } else if(distribution == "Uniform"){
+      
+      x = stats::runif(n) 
+      y <- intercept + beta * x + stats::runif(length(x)) 
+      model_lm <- stats::lm(y ~ x)
+      intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+      beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+      
+      MonteCarlo = list(intercept_mc, beta_mc, x, y)
+      
+      
+      
+    } else if(distribution == "Exponential"){
+      
+      x = stats::rexp(n) 
+      y <- intercept + beta * x + stats::rexp(length(x)) 
+      model_lm <- stats::lm(y ~ x)
+      intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+      beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+      
+      MonteCarlo = list(intercept_mc, beta_mc, x, y)
+      
+      
+      
+    }else if(distribution == "Poisson"){
+      
+      x = stats::rpois(n, lambda = 2)
+      y <- intercept + beta * x + stats::rpois(length(x), lambda = 2) 
+      model_lm <- stats::lm(y ~ x)
+      intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+      beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+      
+      MonteCarlo = list(intercept_mc, beta_mc, x, y)
+      
+    }
+  
     
   }
  
+  MonteCarlo = data.frame(target = y,
+                          beta_1 = MonteCarlo[[1]], 
+                          beta_2 = MonteCarlo[[2]], 
+                          observations = x)
+  
   return(MonteCarlo)
    
 }
 
-simulation_mc = MonteCarlo(intercept = 20, 
-                           beta = 0.6, 
-                           n = 25,
-                           repetitions = 10000)
 
-plot_histogram(simulation_mc[[1]])
-plot_histogram(simulation_mc[[2]])
+
+
+
 
