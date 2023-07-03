86b537ded9b16ff6022c3eb664b5b72eb0b86ee0
LuizPaulo23 <luizpauloueg@gmail.com>
2023-07-03 19:20:05 -0300

    CHANGED: ++ distribuições de probabilidade

--- Front_server.R
+++ Front_server.R
@@ -19,19 +19,28 @@ custom_theme = custom_theme = bslib::bs_theme(bootswatch = "darkly")
 
 # Informação da descrição 
 
-info <- "The Monte Carlo methodology is a statistical technique that involves the use of random sampling to perform simulations and estimate numerical results. The method is named after the famous Monte Carlo casino, known for its games of chance based on random events. In the context of the linear model, Monte Carlo simulation can be applied to obtain estimates of the model parameters (β1 and β2) and evaluate the uncertainty associated with these estimates. Monte Carlo simulation allows for examining multiple realizations of the model based on different input values and obtaining a distribution of results. The application follows the linear model defined as Yᵢ = β₁ + β₂Xᵢ + μᵢ, where Yᵢ represents the dependent variable, Xᵢ is the independent variable, β₁ and β₂ are the model parameters, and μᵢ is the error term with a Gaussian distribution (with mean zero and variance equal to 1)"
+info <- "The Monte Carlo methodology is a statistical technique that involves the use of random sampling to perform simulations and estimate numerical results. The method is named after the famous Monte Carlo casino, known for its games of chance based on random events. In the context of the linear model, Monte Carlo simulation can be applied to obtain estimates of the model parameters (β1 and β2) and evaluate the uncertainty associated with these estimates. Monte Carlo simulation allows for examining multiple realizations of the model based on different input values and obtaining a distribution of results. The application follows the linear model defined as Yᵢ = β₁ + β₂Xᵢ + μᵢ, where Yᵢ represents the dependent variable, Xᵢ is the independent variable, β₁ and β₂ are the model parameters, and μᵢ is the error term."
 
-# Front-end
+# \\\\Front-end
 
 ui <- shiny::fluidPage(
   theme = custom_theme,
   titlePanel("Monte Carlo Simulation"),
   sidebarLayout(
     sidebarPanel(
-      numericInput("intercept", "Intercept:", value = NULL),
+      numericInput("intercept", "Intercept (β1):", value = NULL),
       numericInput("beta", "Parameter (β2):", value = NULL),
-      numericInput("n", "Sample Size:", value = NULL),
-      numericInput("repetitions", "Number of Repetitions (simulation):", value = NULL),
+      selectInput("distribution",
+                  label = "Distribution of X and Residues:",
+                  choices = c("Gaussian", "Uniform", "Exponential", "Poisson: λ = 2"), 
+                  selected = NULL),
+      numericInput("n", "Sample Size:", min = 1,
+                   value = 2),
+      sliderInput("repetitions", "Number of Repetitions (simulation):", 
+                  min = 0, 
+                  max = 5000, 
+                  step = 100, 
+                  value = 100),
       actionButton("simulate", "Simulate"),
       downloadButton("downloadData", "Download Data")
     ),
@@ -66,47 +75,95 @@ ui <- shiny::fluidPage(
 )
 
 
+# \\\\\Back-end 
 
-# Definição do servidor Shiny
 server <- function(input, output) {
   
-# Função Monte Carlo
-MonteCarlo <- function(intercept, beta, n, repetitions) {
+# Função Monte Carlo -----------------------------------------------------------  
+
+MonteCarlo <- function(intercept, beta,distribution,n,repetitions){
     
-    x <- rnorm(n)
-    intercept_mc <- numeric(repetitions)
-    beta_mc <- numeric(repetitions)
+    
+intercept_mc = numeric(repetitions)
+beta_mc = numeric(repetitions)
     
     for (i in 1:repetitions) {
-      y <- intercept + beta * x + rnorm(length(x), mean = 0, sd = 1)
-      model_lm <- lm(y ~ x)
-      intercept_mc[i] <- as.vector(model_lm$coefficients[1])
-      beta_mc[i] <- as.vector(model_lm$coefficients[2])
+      # Definindo a equação 
+      
+      if(distribution == "Gaussian"){
+        
+        x = stats::rnorm(n) 
+        y <- intercept + beta * x + stats::rnorm(length(x)) 
+        model_lm <- stats::lm(y ~ x)
+        intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+        beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+        
+        MonteCarlo = list(intercept_mc, beta_mc, x, y)
+        
+      } else if(distribution == "Uniform"){
+        
+        x = stats::runif(n) 
+        y <- intercept + beta * x + stats::runif(length(x)) 
+        model_lm <- stats::lm(y ~ x)
+        intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+        beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+        
+        MonteCarlo = list(intercept_mc, beta_mc, x, y)
+        
+         } else if(distribution == "Exponential"){
+        
+        x = stats::rexp(n) 
+        y <- intercept + beta * x + stats::rexp(length(x)) 
+        model_lm <- stats::lm(y ~ x)
+        intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+        beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+        
+        MonteCarlo = list(intercept_mc, beta_mc, x, y)
+        
+        
+      }else if(distribution == "Poisson: λ = 2"){
+        
+        x = stats::rpois(n, lambda = 2)
+        y <- intercept + beta * x + stats::rpois(length(x), lambda = 2) 
+        model_lm <- stats::lm(y ~ x)
+        intercept_mc[[i]] <- base::as.vector(model_lm$coefficients[1])
+        beta_mc[[i]] <- base::as.vector(model_lm$coefficients[2])
+        
+        MonteCarlo = list(intercept_mc, beta_mc, x, y)
+        
+      }
+      
+      
     }
     
-    list(intercept_mc, beta_mc)
-  }
+    # MonteCarlo = data.frame(target = y,
+    #                         beta_1 = MonteCarlo[[1]], 
+    #                         beta_2 = MonteCarlo[[2]], 
+    #                         observations = x)
+    
+    return(MonteCarlo)
+    
+}
   
-  # INFOR
-
-output$descricao <- renderText({
- info
-})
-
-  # Função para gerar o gráfico
   
-  output$intercept <- renderPlot({
+# Informação Descritiva --------------------------------------------------------
+
+output$descricao <- renderText({ info })
+
+# Gráfico de dispersão ---------------------------------------------------------
+  
+output$intercept <- renderPlot({
     if (input$simulate > 0) {
       
       mc_data <- MonteCarlo(input$intercept, 
                             input$beta,
+                            input$distribution,
                             input$n,
                             input$repetitions)
       
       media <- mean(mc_data[[1]])
       desvio_padrao <- sd(mc_data[[1]])
       
-  
       ggplot() +
         geom_histogram(data = data.frame(x = mc_data[[1]]), aes(x = x),
                        fill = "gray", 
@@ -116,9 +173,9 @@ output$descricao <- renderText({
                    color = "red",
                    linetype = "dashed",
                    size = 1) +
-        labs(x = "Intercept", 
+        labs(x = "Intercept (β1)", 
              y = "Frequency",
-             title = "Monte Carlo Simulation - Intercept",
+             title = "Monte Carlo Simulation - Intercept (β1)",
              subtitle = paste("Mean:", round(media, 4), "| Standard deviation:", round(desvio_padrao, 4))) +
         theme_bw()
       
@@ -126,48 +183,52 @@ output$descricao <- renderText({
     }
 })
   
-  
-  
-  output$beta <- renderPlot({
-    if (input$simulate > 0) {
-      
-      mc_data <- MonteCarlo(input$intercept, 
-                            input$beta,
-                            input$n,
-                            input$repetitions)
-      
-      media <- mean(mc_data[[2]])
-      desvio_padrao <- sd(mc_data[[2]])
-      
-      # Criar o histograma com ggplot
-      ggplot() +
-        geom_histogram(data = data.frame(x = mc_data[[2]]), aes(x = x),
-                       fill = "gray", 
-                       color = "black",
-                       bins = 30) +
-        geom_vline(xintercept = media,
-                   color = "red",
-                   linetype = "dashed",
-                   size = 1) +
-        labs(x = "Parameter (β2)", 
-             y = "Frequency",
-             title = "Monte Carlo Simulation - Parameter (β2)",
-             subtitle = paste("Mean:", round(media, 4), "| Standard deviation:", round(desvio_padrao, 4))) +
-        theme_bw()
-      
-      
-    }
-  })
-  
-  
-  # Função para fazer o download dos dados
+
+# Parâmetro - beta2 ------------------------------------------------------------
+
+output$beta <- renderPlot({
+  if (input$simulate > 0) {
+    
+    mc_data <- MonteCarlo(input$intercept, 
+                          input$beta,
+                          input$distribution,
+                          input$n,
+                          input$repetitions)
+    
+    media <- mean(mc_data[[2]])
+    desvio_padrao <- sd(mc_data[[2]])
+    
+    # Criar o histograma com ggplot
+    ggplot() +
+      geom_histogram(data = data.frame(x = mc_data[[2]]), aes(x = x),
+                     fill = "gray", 
+                     color = "black",
+                     bins = 30) +
+      geom_vline(xintercept = media,
+                 color = "red",
+                 linetype = "dashed",
+                 size = 1) +
+      labs(x = "Parameter (β2)", 
+           y = "Frequency",
+           title = "Monte Carlo Simulation - Parameter (β2)",
+           subtitle = paste("Mean:", round(media, 4), "| Standard deviation:", round(desvio_padrao, 4))) +
+      theme_bw()
+    
+    
+  }
+})
+
+# Função para fazer o download dos dados ---------------------------------------
+
   output$downloadData <- downloadHandler(
     filename = function() {
       "monte_carlo_data.csv"
     },
     content = function(file) {
       
-      mc_data <- MonteCarlo(input$intercept, input$beta, 
+      mc_data <- MonteCarlo(input$intercept, 
+                            input$beta, 
+                            input$distribution,
                             input$n, 
                             input$repetitions)
       
@@ -179,6 +240,7 @@ output$descricao <- renderText({
   )
 }
 
-# Executar a interface Shiny
+# Executar a interface Shiny ---------------------------------------------------
+
 shinyApp(ui = ui, server = server)
 
